67a6c1be5a2fdce96b67223647e3ab31058a9ab9
mohan.p <mohan.p@codingmart.com>
2023-02-20 11:01:33 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/prepare/components/NavBar/ContatctUs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContatctUs = () => {
+  return <div>ContatctUs</div>;
+};
+
+export default ContatctUs;
